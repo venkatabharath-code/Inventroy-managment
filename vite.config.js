0f1e530f68728d81adfc854a26867276666ab9ae
paladugubharath7 <paladugubharath7@gmail.com>
2026-06-17 11:24:35 +0530

    Fix Vite base config for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Inventroy-managment/',
+  base: '/',
   plugins: [react()],
   build: {
     rollupOptions: {
